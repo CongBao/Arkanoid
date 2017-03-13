afacfabcefd3bd56eed046591be248f8c9c795ab
CongBao <bao_cong@outlook.com>
2017-03-13 18:11:12 +0000

    add logger

--- src/arkanoid/Main.java
+++ src/arkanoid/Main.java
@@ -1,6 +1,11 @@
 package arkanoid;
 
 import com.jme3.system.AppSettings;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * The launch class.
@@ -10,6 +15,17 @@ import com.jme3.system.AppSettings;
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            FileHandler fh = new FileHandler("log.txt");
+            fh.setLevel(Level.ALL);
+            fh.setFormatter(new SimpleFormatter());
+            Logger.getLogger("com.jme3").addHandler(fh);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         AppSettings settings = new AppSettings(true);
         settings.setTitle("Arkanoid");
         settings.setResolution(1280, 720);
